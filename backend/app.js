5738c5a06665580ab2f0b206c37d10c1da21228c
Young wook Jeon <ela87@naver.com>
2021-03-13 00:37:16 +0900

    Create a middleware to handle errors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const errorMiddleware = require('./middlewares/errors');
+
 app.use(express.json());
 
 const products = require('./routes/product');
 
 app.use('/api/v1', products);
+
+// Middleware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,78 +1,71 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 // Create new product => /api/v1/admin/product/new
-exports.newProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        product
-    });
-};
+  res.status(201).json({
+    success: true,
+    product,
+  });
+})
 
 // Get all products => /api/v1/products
-exports.getProducts = async (req, res, next) => {
-    const products = await Product.find();
+exports.getProducts = catchAsyncErrors(async (req, res, next) => {
+  const products = await Product.find();
 
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        products
-    });
-};
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    products,
+  });
+})
 
 // Get single product details => /api/v1/product/:id
-exports.getSingleProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
+exports.getSingleProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        });
-    }
-    res.status(200).json({
-        success: true,
-        product
-    });
-};
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 // Update product => /api/v1/admin/product/:id
-exports.updateProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        });
-    }
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false,
-    });
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-    res.status(200).json({
-        success: true,
-        product
-    });
-};
+  res.status(200).json({
+    success: true,
+    product,
+  });
+})
 
 // Delete product => /api/v1/admin/product/:id
-exports.deleteProduct = async (req, res, next) => {
-    const product = await Product.findById(req.params.id);
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        });
-    }
-    await product.remove();
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  await product.remove();
 
-    res.status(200).json({
-        success: true,
-        message: 'Product is deleted',
-    });
-};
+  res.status(200).json({
+    success: true,
+    message: "Product is deleted",
+  });
+})
